e23f0378c7cfbe056d71ad0715d0fb0452dcc038
akovi.biz <akovi.biz@gmail.com>
2024-05-22 16:56:31 +0200

    Enhancing benchmark and check logs

--- benchmark/new_version/benchmark.h
+++ benchmark/new_version/benchmark.h
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <stdbool.h>
 
 #define DATA_CHUNK_SIZE 1024
 
@@ -18,7 +19,7 @@ char * generate_random_data();
 void create_random_file(const char * path, size_t file_size);
 void read_performance(const char * path, BenchmarkResult * result);
 void write_performance(const char * path, BenchmarkResult * result);
-int check_write_read(const char * path, const char * test_data);
+bool check_write_read(const char * path, const char * test_data);
 void compute_offsets(size_t file_size, size_t chuck_size, size_t num_chunks, size_t offsets[]);
 void setup(const char * folder, size_t N);
 void run_benchmark(const char * folder_path, size_t N);
